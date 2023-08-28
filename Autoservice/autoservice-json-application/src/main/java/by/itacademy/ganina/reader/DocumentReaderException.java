1462f148da71945194d35fa68c49cee4ea404fec
JuliaGanina <_adelaida_@tut.by>
2023-08-28 20:51:14 +0300

    Revert "add AutoserviceApp with txt &json files"
    This reverts commit 7f4df6914e65d3356fa98a589a98f490bf918100.

--- Autoservice/autoservice-json-application/src/main/java/by/itacademy/ganina/reader/DocumentReaderException.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy.ganina.reader;
-
-public class DocumentReaderException extends Exception {
-
-    public DocumentReaderException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
-}
